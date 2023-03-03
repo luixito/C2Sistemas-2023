e82d3ff37ac90d34545e5e22c95781da79f6dfed
luis daniel molina <55844181+luixito@users.noreply.github.com>
2023-03-02 19:37:57 -0600

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+// client/src/App.js
+
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("http://localhost:3001/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{!data ? "Activar cors para ver el cambio" : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
