b76f56f7ad1aaa29d6c80980643589e63f6d11c6
luis daniel molina <55844181+luixito@users.noreply.github.com>
2023-03-02 20:03:27 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
-    fetch("http://localhost:3001/api")
+    fetch("http://ec2-54-183-87-160.us-west-1.compute.amazonaws.com:8080/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
